5a25e5ed58820533988e1d910158ff839985a5d7
Vespertino <Vespertino@C07PC15.informatica.comercio>
2021-11-09 20:32:15 +0100

    add Mano class

---
+++ js/Mano.js
@@ -0,0 +1,47 @@
+const limit = 21;
+
+class Mano {
+    constructor(numCards) {
+        this.numCards = numCards;
+        this.cards = [];
+        this.points = 0;
+    }
+
+    constructor() {
+        this.numCards = 0;
+        this.cards = [];
+        this.points = 0;
+    }
+
+    deleteCards() {
+        this.cards = [];
+        this.numCards = 0;
+    }
+
+    addCard(card) {
+        this.cards.push(card);
+        this.numCards++;
+        this.getPoints();
+    }
+
+    getNumCards() {
+        return this.numCards;
+    }
+
+    getPoints() {
+        let points = 0;
+        this.cards.forEach(card => {
+            points += card.getPoints();
+        });
+        return points;
+    }
+
+    toString() {
+        let str = "";
+        this.cards.forEach(card => {
+            str += card.toString() + " ";
+        });
+        return str;
+    }
+
+}
